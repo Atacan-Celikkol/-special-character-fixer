849d360fded0174218cf299de047f2459d792fbb
Atacan Çelikkol <atacan@ovidos.com>
2019-06-02 21:08:03 +0300

    Functions & Design

--- SpecialCharacterFixer/MainForm.Designer.cs
+++ SpecialCharacterFixer/MainForm.Designer.cs
@@ -28,30 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.progressBar = new MetroFramework.Controls.MetroProgressBar();
+            this.FileNameColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.dropList = new MetroFramework.Controls.MetroListView();
             this.SuspendLayout();
+
+
+            this.Style = MetroFramework.MetroColorStyle.Red;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            //this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(15, 421);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(370, 23);
+            this.progressBar.Style = this.Style;
+            this.progressBar.TabIndex = 1;
+            this.progressBar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.progressBar.Theme = this.Theme;
+            // 
+            // FileNameColumn
+            // 
+            this.FileNameColumn.Text = "File Name";
+            this.FileNameColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.FileNameColumn.Width = 370;
             // 
             // dropList
             // 
+            this.dropList.Alignment = System.Windows.Forms.ListViewAlignment.Left;
             this.dropList.AllowDrop = true;
+            this.dropList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.dropList.CheckBoxes = true;
+            this.dropList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.FileNameColumn});
             this.dropList.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.dropList.ForeColor = System.Drawing.SystemColors.Window;
             this.dropList.FullRowSelect = true;
-            this.dropList.Location = new System.Drawing.Point(30, 86);
+            this.dropList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.dropList.Location = new System.Drawing.Point(15, 65);
             this.dropList.Name = "dropList";
             this.dropList.OwnerDraw = true;
-            this.dropList.Size = new System.Drawing.Size(407, 298);
-            this.dropList.Style = MetroFramework.MetroColorStyle.Lime;
+            this.dropList.Scrollable = false;
+            this.dropList.Size = new System.Drawing.Size(370, 350);
+            this.dropList.Style = this.Style;
             this.dropList.TabIndex = 0;
-            this.dropList.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.dropList.Theme = this.Theme;
             this.dropList.UseCompatibleStateImageBehavior = false;
+            this.dropList.UseCustomBackColor = true;
+            this.dropList.UseCustomForeColor = true;
             this.dropList.UseSelectable = true;
             this.dropList.UseStyleColors = true;
+            this.dropList.View = System.Windows.Forms.View.Details;
             this.dropList.DragDrop += new System.Windows.Forms.DragEventHandler(this.DropList_DragDrop);
+            this.dropList.DragEnter += new System.Windows.Forms.DragEventHandler(this.DropList_DragEnter);
             // 
             // MainForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(467, 415);
+            this.ClientSize = new System.Drawing.Size(400, 459);
+            this.Controls.Add(this.progressBar);
             this.Controls.Add(this.dropList);
             this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
@@ -60,10 +96,7 @@
             this.Padding = new System.Windows.Forms.Padding(27, 83, 27, 28);
             this.Resizable = false;
             this.ShadowType = MetroFramework.Forms.MetroFormShadowType.AeroShadow;
-            this.Style = MetroFramework.MetroColorStyle.Teal;
             this.Text = "Special Character Fixer";
-            this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
-            this.Theme = MetroFramework.MetroThemeStyle.Dark;
             this.TransparencyKey = System.Drawing.Color.Ivory;
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
@@ -71,7 +104,8 @@
         }
 
         #endregion
-
+        private MetroFramework.Controls.MetroProgressBar progressBar;
+        private System.Windows.Forms.ColumnHeader FileNameColumn;
         private MetroFramework.Controls.MetroListView dropList;
     }
 }
--- SpecialCharacterFixer/MainForm.cs
+++ SpecialCharacterFixer/MainForm.cs
@@ -1,12 +1,6 @@
 ﻿using MetroFramework.Forms;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace SpecialCharacterFixer
@@ -20,12 +14,74 @@ namespace SpecialCharacterFixer
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-
         }
 
         private void DropList_DragDrop(object sender, DragEventArgs e)
         {
+            progressBar.Value = 0;
+            dropList.Items.Clear();
+
+            string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var item in filePaths)
+            {
+                var listItem = new ListViewItem(Path.GetFileName(item));
+                listItem.ToolTipText = item;
+                dropList.Items.Add(listItem);
+            }
+
+            for (int i = 0; i < filePaths.Length; i++)
+            {
+                var fileText = SpecialCharacterFixer(File.ReadAllText(filePaths[i]));
+                File.WriteAllText(filePaths[i], fileText);
+                dropList.Items[i].Checked = true;
+                progressBar.Value += 100 / filePaths.Length;
+            }
+            progressBar.Value = 100;
             return;
+        }
+
+        private void DropList_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+            return;
+        }
+
+        private string SpecialCharacterFixer(string text)
+        {
+            var fixedText = "";
+            var chars = text.ToCharArray();
+            foreach (var chr in chars)
+            {
+                switch (chr)
+                {
+                    case 'ð':
+                        fixedText += 'ğ';
+                        break;
+                    case 'Ð':
+                        fixedText += 'Ğ';
+                        break;
+
+                    case 'ý':
+                        fixedText += 'ı';
+                        break;
+                    case 'Ý':
+                        fixedText += 'İ';
+                        break;
+
+                    case 'þ':
+                        fixedText += 'ş';
+                        break;
+                    case 'Þ':
+                        fixedText += 'Ş';
+                        break;
+
+                    default:
+                        fixedText += chr;
+                        break;
+                }
+            }
+
+            return fixedText;
         }
     }
 }
